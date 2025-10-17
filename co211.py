549c24fc9131976ebf53a10ae517dd5f30c8e843
krishna-priya-03 <kpriye74@gmail.com>
2025-10-17 12:49:50 +0530

    Add files via upload

---
+++ co211.py
@@ -0,0 +1,16 @@
+area_square = lambda  side : side * side
+area_rectangle= lambda  length, width : length*width
+area_triangle = lambda s,a,b,c :(s*(s-a)*(s-b)*(s-c))**0.5
+a=int(input("enter the number a:"))
+b=int(input("enter the number b:"))
+c=int(input("enter the number c:"))
+s = (a + b + c) / 2
+print(area_square (a))
+print(area_rectangle(a,b))
+print(area_triangle(s,a,b,c))
+x=lambda b:b+3
+print(x(3))
+
+
+
+
