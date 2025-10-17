19d54ac71762171d879cd81b5952521e7738ac51
krishna-priya-03 <kpriye74@gmail.com>
2025-10-17 12:26:58 +0530

    Add files via upload

---
+++ co51.py
@@ -0,0 +1,10 @@
+
+file=open("lekshmi.txt","r")
+content=file.readlines()
+linelist=[]
+for i in content:
+       print(i)
+       line=i.strip()
+       linelist.append(line)
+print(linelist)
+file.close()
