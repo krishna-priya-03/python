eed3b5062e35868b1401ddcda74ef753e2458434
krishna-priya-03 <kpriye74@gmail.com>
2025-10-30 02:36:40 -0700

    Add files via upload

---
+++ co3/main.py
@@ -0,0 +1,15 @@
+from graphics import rectangle , circle
+from graphics.threeDgraphics import cuboid , sphere
+
+print("rectangle area:",rectangle.area(10 , 5))
+print("rectangle perimeter:",rectangle.perimeter(10 , 5))
+
+print("circle area:",circle.area(7))
+print("circle perimeter:",circle.perimeter(7))
+
+print("cuboid surface area:",cuboid.surface_area(2, 3 ,4))
+print("cuboid  perimeter:",cuboid.perimeter(2 , 3, 4))
+
+print("sphere surface  area:",sphere.surface_area(5))
+print("sphere volume:",sphere . volume(5))
+
